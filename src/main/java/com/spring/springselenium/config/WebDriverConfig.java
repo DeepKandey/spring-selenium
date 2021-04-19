3afe1b84b4ade2338fc3ea1fddf732dcabfa889c
Deepak Kumar Rai <Deepakggsipu@gmail.com>
2021-04-19 17:57:15 +0530

    @ConditionMissingBean and @ConditionalOnProperty added

--- src/main/java/com/spring/springselenium/config/WebDriverConfig.java
+++ src/main/java/com/spring/springselenium/config/WebDriverConfig.java
@@ -3,11 +3,15 @@ package com.spring.springselenium.config;
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
 
 @Lazy
 @Configuration
@@ -16,7 +20,17 @@ public class WebDriverConfig {
   @Value("${default.timeout:30}")
   private int timeout;
 
+  @Primary
   @Bean
+  @ConditionalOnProperty(name = "browser", havingValue = "firefox")
+  public WebDriver firefoxDriver() {
+    System.setProperty(
+        "webdriver.gecko.driver", "C:\\Users\\deepa\\Downloads\\BrowserDrivers\\geckodriver.exe");
+    return new FirefoxDriver();
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
   public WebDriver chromeDriver() {
     System.setProperty(
         "webdriver.chrome.driver", "C:\\Users\\deepa\\Downloads\\BrowserDrivers\\chromedriver.exe");
--- src/test/java/com/spring/springselenium/googleTest/GoogleTest.java
+++ src/test/java/com/spring/springselenium/googleTest/GoogleTest.java
@@ -3,11 +3,12 @@ package com.spring.springselenium.googleTest;
 import com.spring.springselenium.SpringBaseTestNGTest;
 import com.spring.springselenium.page.google.GooglePage;
 import com.spring.springselenium.util.ScreenshotUtil;
-import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class GoogleTest extends SpringBaseTestNGTest {
 
@@ -15,12 +16,13 @@ public class GoogleTest extends SpringBaseTestNGTest {
   @Lazy @Autowired private ScreenshotUtil screenshotUtil;
 
   @Test
-  public void googleTest() throws IOException {
+  public void googleTest() throws IOException, InterruptedException {
     this.googlePage.goTo();
     Assert.assertTrue(this.googlePage.isAt());
 
     this.googlePage.getSearchComponent().search("spring boot");
     Assert.assertTrue(this.googlePage.getSearchResult().isAt());
+    Thread.sleep(3000);
     Assert.assertTrue(this.googlePage.getSearchResult().getCount() > 2);
     //   this.screenshotUtil.takeScreenshot();
   }
